5741191492ef5c717d67a29ec950f07563923f6b
JaykumarPatel4802 <patel.jay4802@gmail.com>
2024-11-06 17:31:09 -0600

    [Formulation] Add formulation for more problems

--- create_formulation_files.py
+++ create_formulation_files.py
@@ -3,7 +3,7 @@
 
 
 problems = [
-    "Two Sum", "Valid Anagram", "Product of Array Except Self", "Top K Frequent Elements", "Longest Consecutive Suquence", "Valid Palindrome", "Best Time to Buy and Sell Stock", "Container with Most Water", "Two Sum II INput Array Is Sorted", "3Sum", "Trapping Rain Water", "Invert Binary Tree", "Diameter of Binary Tree", "Binary Tree Right Side View", "Lowest Common Ancestor of a Binary Search Tree", "Binary tree Maximum Path Sum", "Serialize and Deserialize Binary Tree", "Number of Islands", "Course Schedule", "Rotting Oranges", "Word Search", "Combination Sum", "Word Ladder", "N Queens", "Climbing Stairs", "Min Cost Climbing Stairs", "House Robber", "Coin Change", "Palindromic Substrings", "Maximum Product Subarray"
+    "Two Sum", "Valid Anagram", "Product of Array Except Self", "Top K Frequent Elements", "Longest Consecutive Suquence", "Valid Palindrome", "Best Time to Buy and Sell Stock", "Container with Most Water", "Two Sum II Input Array Is Sorted", "3Sum", "Trapping Rain Water", "Invert Binary Tree", "Diameter of Binary Tree", "Binary Tree Right Side View", "Lowest Common Ancestor of a Binary Search Tree", "Binary tree Maximum Path Sum", "Serialize and Deserialize Binary Tree", "Number of Islands", "Course Schedule", "Rotting Oranges", "Word Search", "Combination Sum", "Word Ladder", "N Queens", "Climbing Stairs", "Min Cost Climbing Stairs", "House Robber", "Coin Change", "Palindromic Substrings", "Maximum Product Subarray"
 ] 
 
 for idx, problem in enumerate(problems):
